b5bd7bcab6385fb671d19c5ada83be6f26dc14ba
jiutianhuanpei <shenhongbang@163.com>
2017-12-07 22:11:12 +0800

    添加注释

--- ContactManager/ContactsModule/HYMediator+ContactsModule.h
+++ ContactManager/ContactsModule/HYMediator+ContactsModule.h
@@ -10,8 +10,27 @@
 
 @interface HYMediator (ContactsModule)
 
+
+/**
+ 请求读取通讯录权限
+
+ @param handler 回调
+ */
 - (void)HYMediator_applyForPermission:(void(^)(BOOL granted, NSError *error))handler;
+
+/**
+ 读取所有的，符合规则的成员
+ 其手机号符合 1[0-9]{10}，
+
+ @return 联系人数组
+ */
 - (NSArray <NSDictionary *>*)HYMediator_fetchAllLocalContacts;
+
+/**
+ 当通讯录有改变时的通知回调
+
+ @param handler 回调
+ */
 - (void)HYMediator_contactsDidChanged:(dispatch_block_t)handler;
 
 @end
